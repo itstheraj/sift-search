c85a707fbf73dc91a3822cf451ef1fd00fb56872
Suraj Mirpuri <mirpurisuraj1@gmail.com>
2026-07-08 22:37:21 -0700

    Derive version from package metadata

--- src/sift/__init__.py
+++ src/sift/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version as _version
+
+try:
+    __version__ = _version("sift-search-kde")
+except PackageNotFoundError:  # running from a source tree that was never installed
+    __version__ = "0.0.0+unknown"
